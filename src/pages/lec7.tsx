aa86387354afae8efbb2a0bd3d99ca10e1344fec
Seongwoo_Lukaid <crescent70004536@gmail.com>
2024-03-13 03:19:47 +0900

    tiny

--- src/pages/lec7.tsx
+++ src/pages/lec7.tsx
@@ -6,7 +6,7 @@
 export default function Lec7() {
   return (
     <main className="bg-gray-100 md:bg-green-100 h-screen flex items-center justify-center p-5">
-      <div className="flex flex-col md:flex-row gap-2 bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm *:outline-none has-[:invalid]:ring-red-200 has-[:invalid]:ring has-[:focus:invalid]:bg-red-100 ring ring-transparent transition-shadow">
+      <div className="flex flex-col md:flex-row gap-2 bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm *:outline-none ring ring-transparent transition-shadow has-[:invalid]:ring-red-200 has-[:invalid]:ring has-[:focus:invalid]:bg-red-100">
         <input
           className="w-full rounded-full h-10 bg-gray-200 pl-5 py-3 ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow focus:placeholder:drop-shadow focus:invalid:bg-red-100 focus:invalid:ring-orange-500"
           type="text"
